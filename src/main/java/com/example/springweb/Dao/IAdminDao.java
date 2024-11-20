d9153d9aa5c8b93eb4e4b62a321e1f003bc16c3d
TDCQCX <3290264220@qq.com>
2024-11-20 09:08:28 +0800

    修改了MyBatis的映射（未运行，不知好坏）

--- src/main/java/com/example/springweb/Dao/IAdminDao.java
+++ src/main/java/com/example/springweb/Dao/IAdminDao.java
@@ -2,10 +2,13 @@ package com.example.springweb.Dao;
 
 import com.example.springweb.Entity.AdminTable;
 
+import java.util.List;
+
 public interface IAdminDao {
 //    增加管理员账号
     boolean addAdmin(AdminTable admin);
 //    增删管理员账号
 //    更改管理员账号
 //    查询管理员账号
+    List<AdminTable> selectAllAdmin();
 }
