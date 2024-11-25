c524b9b7d6bace30a26c41a17e0759648aa6ff26
alksdjdawioj <151594605+alksdjdawioj@users.noreply.github.com>
2024-11-25 19:26:05 +0800

    前端文件

--- JavaWeb/src/main/java/com/example/javaweb/JavaWebApplication.java
+++ JavaWeb/src/main/java/com/example/javaweb/JavaWebApplication.java
@@ -2,6 +2,7 @@ package com.example.javaweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
 public class JavaWebApplication {
