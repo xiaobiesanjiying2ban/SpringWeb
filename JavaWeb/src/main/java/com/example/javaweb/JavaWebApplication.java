7b7e6f6257a33629d7eb1f09262e8889740e15fb
dalao <3056178883@qq.com>
2024-12-19 10:10:58 +0800

    大黄的修改

--- JavaWeb/src/main/java/com/example/javaweb/JavaWebApplication.java
+++ JavaWeb/src/main/java/com/example/javaweb/JavaWebApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaWebApplication {
 	public static void main(String[] args) {
+		// 你看个锤子
 		SpringApplication.run(JavaWebApplication.class, args);
 	}
 }
